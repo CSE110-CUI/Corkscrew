bfa91f5c6f06f11cdbfb4594df8b21c7667b65d4
Chris Weller <chriskweller@gmail.com>
2013-05-19 01:22:40 -0700

    Initial swipe view implemented.

--- WineAppCS110/src/edu/cui/wineapp/WineInfo.java
+++ WineAppCS110/src/edu/cui/wineapp/WineInfo.java
@@ -1,13 +1,17 @@
 package edu.cui.wineapp;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.*;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.*;
 
 import java.io.IOException;
@@ -15,14 +19,33 @@ import java.io.InputStream;
 import java.net.URL;
 import java.util.ArrayList;
 
-public class WineInfo extends Activity {
+public class WineInfo extends FragmentActivity {
 
     ArrayList<String> foodNames;
+
+    ViewPager mPager;
+    pgrAdapter mAdapter;
+    static ArrayList<String> Cheeses;
+    static int NUM_ITEMS = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Cheeses = new ArrayList<String>();
+
+        Cheeses.add("One");
+        Cheeses.add("Two");
+        Cheeses.add("Three");
+
         setContentView(R.layout.activity_wine_info);
+
+
+        mAdapter = new pgrAdapter(getSupportFragmentManager());
+
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(mAdapter);
+
 
         foodNames = new ArrayList<String>();
 
@@ -57,11 +80,11 @@ public class WineInfo extends Activity {
             foodNames.add(myFoods.get(count).getName());
         }
 
-
+/*
         ListView foodList = (ListView) findViewById(R.id.listView);
         foodList.setAdapter(new ArrayAdapter<String>
                 (this, android.R.layout.simple_list_item_1, foodNames));
-
+*/
 
     }
 
@@ -106,4 +129,76 @@ public class WineInfo extends Activity {
         }
     }
 
+    public static class pgrAdapter extends FragmentPagerAdapter {
+        public pgrAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return ArrayListFragment.newInstance(position);
+        }
+    }
+
+    public static class ArrayListFragment extends ListFragment {
+        int mNum;
+
+        /**
+         * Create a new instance of CountingFragment, providing "num"
+         * as an argument.
+         */
+        static ArrayListFragment newInstance(int num) {
+            ArrayListFragment f = new ArrayListFragment();
+
+            // Supply num input as an argument.
+            Bundle args = new Bundle();
+            args.putInt("num", num);
+            f.setArguments(args);
+
+            return f;
+        }
+
+        /**
+         * When creating, retrieve this instance's number from its arguments.
+         */
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            mNum = getArguments() != null ? getArguments().getInt("num") : 1;
+        }
+
+        /**
+         * The Fragment's UI is just a simple text view showing its
+         * instance number.
+         */
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View v = inflater.inflate(R.layout.fragment_pager_list, container, false);
+            View tv = v.findViewById(R.id.text);
+            ((TextView) tv).setText("Fragment #" + mNum);
+            return v;
+        }
+
+        @Override
+        public void onActivityCreated(Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
+            setListAdapter(new ArrayAdapter<String>(getActivity(),
+                    android.R.layout.simple_list_item_1, Cheeses));
+        }
+
+        @Override
+        public void onListItemClick(ListView l, View v, int position, long id) {
+            Log.i("FragmentList", "Item clicked: " + id);
+        }
+    }
+
 }
+
+
+
